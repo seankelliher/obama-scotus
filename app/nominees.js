339eda5e790731a550c8208b9a25c3a9835a1301
Sean Kelliher <sbkelliher@icloud.com>
2021-02-10 00:13:26 -0500

    Add 'nominees.js', contains arrays with photos, titles, text.

---
+++ app/nominees.js
@@ -0,0 +1,175 @@
+const scene1 = {
+    image: "obama-chats-with-justices.jpg",
+    alt: "Barack Obama chats with Supreme Court justices",
+    position: "bubble2",
+    headline: "How Obama Shaped the Supreme Court",
+    text: [
+        "The people he opposed",
+        "and nominated."
+    ].join(" "),
+    credit: "Photo: Pete Souza",
+    source: [
+        "https://www.flickr.com/",
+        "photos/changedotgov/3198083750"
+    ].join("")
+};
+
+const scene2 = {
+    image: "john-roberts-sworn-in.jpg",
+    alt: "John Roberts sworn in as a Supreme Court justice",
+    position: "bubble3",
+    headline: "John Roberts",
+    text: [
+        "In 2005, Senator Obama of Illinois opposes George W. Bush's",
+        "Supreme Court nominee John Roberts.",
+        "Roberts is confirmed anyway and later becomes Chief Justice."
+    ].join(" "),
+    credit: "Photo: Paul Morse",
+    source: [
+        "https://catalog.archives.gov/",
+        "id/12165919"
+    ].join("")
+};
+
+const scene3 = {
+    image: "bush-nominates-samuel-alito.jpg",
+    alt: "George Bush with Samuel Alito at announcement",
+    position: "bubble4",
+    headline: "Samuel Alito",
+    text: [
+        "In 2006, Senator Obama opposes Bush's next Supreme Court nominee,",
+        "Samuel Alito. Like Roberts, Alito is also confirmed."
+    ].join(" "),
+    credit: "Photo: Paul Morse",
+    source: [
+        "https://catalog.archives.gov/",
+        "id/75598005"
+    ].join("")
+};
+
+const scene4 = {
+    image: "obama-biden-meet-sotamayor-",
+    alt: "Barack Obama and Joe Biden meet Sonia Sotomayor about nomination",
+    position: "bubble3",
+    headline: "Sonia Sotomayor",
+    text: [
+        "In May 2009, President Obama nominates Sonia Sotomayor",
+        "of the US Court of Appeals for the Second Circuit",
+        "to the Supreme Court.",
+        "She is confirmed and replaces the retiring David H. Souter.",
+        "Sotomayor was one of four finalists for the job; all women."
+    ].join(" "),
+    credit: "Photo: Pete Souza",
+    source: [
+        "https://www.flickr.com/",
+        "photos/obamawhitehouse/3567253753/"
+    ].join("")
+};
+
+const scene5 = {
+    image: "obama-meets-kagan",
+    alt: "Barack Obama meet Elena Kagan about nomination",
+    position: "bubble4",
+    headline: "Elena Kagan",
+    text: [
+        "In May 2010, President Obama nominates Elena Kagan,",
+        "the Solicitor General of the United States,",
+        "to the Supreme Court.",
+        "She is confirmed and replaces the retiring John Paul Stevens.",
+        "Kagan was a candidate to replace Justice Souter in 2009."
+    ].join(" "),
+    credit: "Photo: Pete Souza",
+    source: [
+        "https://www.flickr.com/",
+        "photos/obamawhitehouse/4594663321/"
+    ].join("")
+};
+
+const scene6 = {
+    image: "antonin-scalia-smiles.jpg",
+    alt: "Antonin Scalia smiles",
+    position: "bubble1",
+    headline: "Antonin Scalia",
+    text: [
+        "In February, 2016, Justice Antonin Scalia is found dead",
+        "while vacationing at Cibolo Creek Ranch near Marfa, Texas."
+    ].join(" "),
+    credit: "Photo: Stephen Masker",
+    source: [
+        "https://www.flickr.com/",
+        "photos/73424305@N00/4668514068/"
+    ].join("")
+};
+
+const scene7 = {
+    image: "obama-biden-nominate-garland.jpg",
+    alt: "Barack Obama and Joe Biden with Merrick Garland at announcement",
+    position: "bubble2",
+    headline: "Merrick Garland",
+    text: [
+        "In March, Obama nominates Merrick Garland,",
+        "Chief Judge of the US Court of Appeals",
+        "for the District of Columbia Circuit to replace Justice Scalia."
+    ].join(" "),
+    credit: "Photo: Pete Souza",
+    source: [
+        "https://obamawhitehouse.archives.gov/blog/2016/03/16/",
+        "what-theyre-saying-about-president-obamas-supreme-court-nominee"
+    ].join("")
+};
+
+const scene8 = {
+    image: "us-capitol-building.jpg",
+    alt: "exterior of the United States Capitol building",
+    position: "bubble4",
+    headline: "Mitch McConnell",
+    text: [
+        "Senate Majority Leader Mitch McConnell refuses to proceed",
+        "with a confirmation hearing,",
+        "citing that Obama is in his final year in office.",
+        "Judge Garland's nomination is held open for 293 days.",
+        "It expires on January 3, 2017, at the end of the 114th Congress."
+    ].join(" "),
+    credit: "Photo: Greg Willis",
+    source: [
+        "https://commons.wikimedia.org/",
+        "IS HERE"
+    ].join("wiki/File:US_Capitol_Dome_%26_Senate_(4824784842).jpg")
+};
+
+const scene9 = {
+    image: "trump-nominates-gorsuch.jpg",
+    alt: "Donald Trump with Neil Gorsuch and his wife at announcement",
+    position: "bubble3",
+    headline: "Neil Gorsuch",
+    text: [
+        "President Trump wins the election and nominates Judge Neil Gorsuch",
+        "of the U.S. Court of Appeals for the Tenth Circuit.",
+        "Judge Gorsuch is confirmed by a vote of 54–45.",
+        "He assumes Justice Scalia's position."
+    ].join(" "),
+    credit: "White House photographer",
+    source: [
+        "https://twitter.com/",
+        "POTUS45/status/826643298022522880"
+    ].join("")
+};
+
+const scene10 = {
+    image: "obama-congratulates-kagan.jpg",
+    alt: "Obama embraces Kagan in Oval Office and she is confirmed",
+    position: "bubble3",
+    headline: "Credits",
+    text: [
+        "Barack Obama White House photos: Pete Souza",
+        "George W. Bush White House photos: Paul Morse",
+        "Capitol photo: Greg Willis",
+        "Antonin Scalia photo: Stephen Masker",
+        "Trump White House photos: White House photographer"
+    ].join(" "),
+    credit: "Photo: Pete Souza",
+    source: [
+        "https://www.flickr.com/",
+        "photos/obamawhitehouse/5307109825/"
+    ].join("")
+};
