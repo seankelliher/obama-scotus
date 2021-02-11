77b0883e0df1b0bb81104061417ea5bf2d2f3a9b
Sean Kelliher <sbkelliher@icloud.com>
2021-02-11 16:59:12 -0500

    Add 'import'. Add 'event listener' for clicks and 'count' variable.

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-//import something...
+import {createScene} from "./app/scenes.js";
 
-//After DOM loads...
+//When DOM loads, invoke function.
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
-    //do something...
 
+    let count;
+    count = 0;
+    createScene(count);//loads first scene (Obama with justices).
+
+    //Get the figure.
+    const figure = document.querySelector("figure");
+
+    //Listen for clicks.
+    figure.addEventListener("click", function () {
+        count = count + 1;
+        createScene(count);
+    });
 });
-//export something...
