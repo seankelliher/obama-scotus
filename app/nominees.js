068ad6c72d8c67e2f81a679a37277849ea05012f
Sean Kelliher <sbkelliher@icloud.com>
2021-02-18 23:21:55 -0500

    Rename 'image' and 'alt' properties to 'panoImage' and 'panoAlt'. Add 'thumbImage' and 'thumbAlt' properties. Remove 'credit' and 'source' properties. Various small text changes.

--- app/nominees.js
+++ app/nominees.js
@@ -1,171 +1,146 @@
 const nomineeInfo = [
     {
-        image: "obama-chats-with-justices",
-        alt: "Barack Obama chats with Supreme Court justices",
+        panoImage: "obama-chats-with-justices",
+        panoAlt: "Barack Obama chats with Supreme Court justices",
         scene: 0,
         headline: "Barack Obama and the Justices",
         paragraph: [
             "As a Senator from Illinois and 44th President,",
             "Obama worked to shape the Supreme Court.",
-            "He both won and lost along the way."
+            "He both won and lost along the way. Photo: Pete Souza."
         ].join(" "),
-        credit: "Photo: Pete Souza",
-        source: [
-            "https://www.flickr.com/",
-            "photos/changedotgov/3198083750"
-        ].join("")
+        thumbImage: "transparent-image", //barack-obama-portrait1
+        thumbAlt: "portrait photo of Barack Obama as Senator"
     },
     {
-        image: "john-roberts-sworn-in",
-        alt: "John Roberts sworn in as a Supreme Court justice",
+        panoImage: "john-roberts-sworn-in",
+        panoAlt: "John Roberts sworn in as a Supreme Court justice",
         scene: 1,
         headline: "John Roberts",
         paragraph: [
             "In 2005, Senator Obama opposes George W. Bush's",
-            "Supreme Court nominee John Roberts.",
-            "Roberts is confirmed anyway and later becomes Chief Justice."
+            "Supreme Court nominee John Roberts. Roberts is confirmed anyway",
+            "and later becomes Chief Justice. Photo: Paul Morse."
         ].join(" "),
-        credit: "Photo: Paul Morse",
-        source: [
-            "https://catalog.archives.gov/",
-            "id/12165919"
-        ].join("")
+        thumbImage: "john-roberts-portrait",
+        thumbAlt: "portrait photo of John Roberts"
     },
     {
-        image: "bush-nominates-samuel-alito",
-        alt: "George Bush with Samuel Alito at announcement",
+        panoImage: "bush-nominates-samuel-alito",
+        panoAlt: "George Bush with Samuel Alito at his nomination announcement",
         scene: 2,
         headline: "Samuel Alito",
         paragraph: [
             "In 2006, Senator Obama opposes Bush's next Supreme Court nominee,",
-            "Samuel Alito. Like Roberts, Alito is also confirmed."
+            "Samuel Alito. Like Roberts, Alito is also confirmed.",
+            "Photo: Paul Morse."
         ].join(" "),
-        credit: "Photo: Paul Morse",
-        source: [
-            "https://catalog.archives.gov/",
-            "id/75598005"
-        ].join("")
+        thumbImage: "samuel-alito-portrait",
+        thumbAlt: "portrait photo of Samuel Alito"
     },
     {
-        image: "obama-biden-meet-sotamayor",
-        alt: "Barack Obama and Joe Biden meet Sonia Sotomayor about nomination",
+        panoImage: "obama-biden-meet-sotamayor",
+        panoAlt: "Barack Obama and Joe Biden interview Sonia Sotomayor",
         scene: 3,
         headline: "Sonia Sotomayor",
         paragraph: [
             "In May 2009, now President Obama nominates Sonia Sotomayor",
-            "of the US Court of Appeals for the Second Circuit",
-            "to the Supreme Court.",
+            "of the US Court of Appeals for the Second Circuit.",
             "She is confirmed and replaces retiring Justice David H. Souter.",
-            "Sotomayor was one of four finalists for the job; all women."
+            "Sotomayor was among four finalists for the job; all women.",
+            "Photo: Pete Souza."
         ].join(" "),
-        credit: "Photo: Pete Souza",
-        source: [
-            "https://www.flickr.com/",
-            "photos/obamawhitehouse/3567253753/"
-        ].join("")
+        thumbImage: "sonia-sotamayor-portrait",
+        thumbAlt: "portrait photo of Sonia Sotomayor"
     },
     {
-        image: "obama-meets-kagan",
-        alt: "Barack Obama meet Elena Kagan about nomination",
+        panoImage: "obama-meets-kagan",
+        panoAlt: "Barack Obama interviews Elena Kagan",
         scene: 4,
         headline: "Elena Kagan",
         paragraph: [
             "In May 2010, President Obama nominates Elena Kagan,",
-            "the Solicitor General of the United States,",
-            "to the Supreme Court.",
+            "the Solicitor General of the United States.",
             "She is confirmed and replaces retiring Justice John Paul Stevens.",
-            "Kagan was a candidate to replace Justice Souter in 2009."
+            "Kagan was a candidate to replace Justice Souter in 2009.",
+            "Photo: Pete Souza."
         ].join(" "),
-        credit: "Photo: Pete Souza",
-        source: [
-            "https://www.flickr.com/",
-            "photos/obamawhitehouse/4594663321/"
-        ].join("")
+        thumbImage: "elena-kagan-portrait",
+        thumbAlt: "portrait photo of Elena Kagan"
     },
     {
-        image: "antonin-scalia-smiles",
-        alt: "Antonin Scalia smiles",
+        panoImage: "antonin-scalia-smiles",
+        panoAlt: "Antonin Scalia smiles",
         scene: 5,
         headline: "Antonin Scalia",
         paragraph: [
             "In February of 2016, Justice Antonin Scalia is found dead",
-            "while vacationing at Cibolo Creek Ranch near Marfa, Texas."
+            "while vacationing at Cibolo Creek Ranch near Marfa, Texas.",
+            "Photo: Stephen Masker."
         ].join(" "),
-        credit: "Photo: Stephen Masker",
-        source: [
-            "https://www.flickr.com/",
-            "photos/73424305@N00/4668514068/"
-        ].join("")
+        thumbImage: "antonin-scalia-portrait",
+        thumbAlt: "portrait photo of Antonin Scalia"
     },
     {
-        image: "obama-biden-nominate-garland",
-        alt: "Barack Obama and Joe Biden with Merrick Garland at announcement",
+        panoImage: "obama-biden-nominate-garland",
+        panoAlt: "Obama and Biden with Garland at his nomination announcement",
         scene: 6,
         headline: "Merrick Garland",
         paragraph: [
             "In March, Obama nominates Merrick Garland,",
             "Chief Judge of the US Court of Appeals",
-            "for the District of Columbia Circuit, to replace Justice Scalia."
+            "for the District of Columbia Circuit.",
+            "Photo: Pete Souza."
         ].join(" "),
-        credit: "Photo: Pete Souza",
-        source: [
-            "https://obamawhitehouse.archives.gov/blog/2016/03/16/",
-            "what-theyre-saying-about-president-obamas-supreme-court-nominee"
-        ].join("")
+        thumbImage: "merrick-garland-portrait",
+        thumbAlt: "portrait photo of Merrick Garland"
     },
     {
-        image: "us-capitol-building",
-        alt: "exterior of the United States Capitol building",
+        panoImage: "us-capitol-building",
+        panoAlt: "exterior of the United States Capitol building",
         scene: 7,
         headline: "Mitch McConnell",
         paragraph: [
-            "But Senate Majority Leader Mitch McConnell refuses to proceed",
-            "with a confirmation hearing,",
-            "citing that Obama is in his final year in office.",
-            "Judge Garland's nomination is held open for 293 days.",
-            "It expires on January 3, 2017, at the end of the 114th Congress."
+            "But Senate Majority Leader Mitch McConnell refuses to hold",
+            "a confirmation hearing,",
+            "citing Obama's final year in office.",
+            "Judge Garland's nomination remains open for 293 days.",
+            "expiring on January 3, 2017, as the 114th Congress closes.",
+            "Photo: Greg Willis."
         ].join(" "),
-        credit: "Photo: Greg Willis",
-        source: [
-            "https://commons.wikimedia.org/",
-            "IS HERE"
-        ].join("wiki/File:US_Capitol_Dome_%26_Senate_(4824784842).jpg")
+        thumbImage: "mitch-mcconnell-portrait",
+        thumbAlt: "portrait photo of Mitch McConnell"
     },
     {
-        image: "trump-nominates-gorsuch",
-        alt: "Donald Trump with Neil Gorsuch and his wife at announcement",
+        panoImage: "trump-nominates-gorsuch",
+        panoAlt: "Trump with Neil Gorsuch at his nomination announcement",
         scene: 8,
         headline: "Neil Gorsuch",
         paragraph: [
-            "Donald Trump is elected and nominates Judge Neil Gorsuch",
+            "Donald Trump wins and nominates Judge Neil Gorsuch",
             "of the U.S. Court of Appeals for the Tenth Circuit.",
             "Gorsuch is confirmed by a vote of 54–45",
-            "and assumes Justice Scalia's position April, 2017."
+            "and assumes Justice Scalia's position in April 2017.",
+            "Photo: White House staff."
         ].join(" "),
-        credit: "White House photographer",
-        source: [
-            "https://twitter.com/",
-            "POTUS45/status/826643298022522880"
-        ].join("")
+        thumbImage: "neil-gorsuch-portrait",
+        thumbAlt: "portrait photo of Neil Gorsuch"
     },
     {
-        image: "obama-congratulates-kagan",
-        alt: "Obama embraces Kagan in Oval Office and she is confirmed",
+        panoImage: "obama-congratulates-kagan",
+        panoAlt: "Obama embraces Kagan in Oval Office and she is confirmed",
         scene: 9,
-        headline: "Photographs",
+        headline: "Additional Photographs",
         paragraph: [
-            "(1) Bush years: Paul Morse.",
-            "(2) Obama years: Pete Souza.",
-            "(3) Antonin Scalia photo: Stephen Masker.",
-            "(4) Capitol photo: Greg Willis.",
-            "(5) Trump years: White House photographer.",
-            "Here: Obama congratulates newly confirmed Justice Elena Kagan."
+            "(A) Judicial portraits: Steve Petteway;",
+            "except Justice Gorsuch: Franz Jantzen, and",
+            "Judge Garland: US Court of Appeals for the DC Circuit.",
+            "(B) Portrait of Senator McConnell: his staff.",
+            "(C) Above: Obama congratulates a confirmed Elena Kagan.",
+            "Photo: Pete Souza."
         ].join(" "),
-        credit: "Photo: Pete Souza",
-        source: [
-            "https://www.flickr.com/",
-            "photos/obamawhitehouse/5307109825/"
-        ].join("")
+        thumbImage: "transparent-image", //barack-obama-portrait2
+        thumbAlt: "portrait photo of Barack Obama as President"
     }
 ];
 
