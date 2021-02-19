fdde2037edc52b2f47ffdb1b9c55c93e9d6d723c
Sean Kelliher <sbkelliher@icloud.com>
2021-02-18 23:26:17 -0500

    Remove consts 'img' and 'section'. Add 'panoImg' and 'thumbImg'.

--- app/scenes.js
+++ app/scenes.js
@@ -4,20 +4,19 @@ function createScene(newScene) {
     "use strict";
 
     //Get elements.
-    const img = document.querySelector("img");
+    const panoImg = document.querySelector("#pano img");
     const hline = document.querySelector("#hline");
     const ptext = document.querySelector("#ptext");
-    const section = document.querySelector("section");
+    const thumbImg = document.querySelector("#thumb img");
 
     //Place images, attributes, or text.
-    img.src = `images/${nomineeInfo[newScene].image}.jpg`;
-    img.alt = `${nomineeInfo[newScene].alt}`;
-    img.dataset.sceneNumber = `${nomineeInfo[newScene].scene}`;
+    panoImg.src = `images/${nomineeInfo[newScene].panoImage}.jpg`;
+    panoImg.alt = `${nomineeInfo[newScene].panoAlt}`;
+    panoImg.dataset.sceneNumber = `${nomineeInfo[newScene].scene}`;
     hline.textContent = `${nomineeInfo[newScene].headline}`;
     ptext.textContent = `${nomineeInfo[newScene].paragraph}`;
-
-    //Place position.
-    section.id = `${nomineeInfo[newScene].position}`;
+    thumbImg.src = `images/${nomineeInfo[newScene].thumbImage}.png`;
+    thumbImg.alt = `${nomineeInfo[newScene].thumbAlt}`;
 }
 
 export {createScene};
