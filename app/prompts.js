71a62ef683f36e09eb4b3276c83d74a0fc48db8c
Sean Kelliher <sbkelliher@icloud.com>
2021-02-20 11:15:24 -0500

    Add 'prompts.js' module, makes icons visible or invisible.

---
+++ app/prompts.js
@@ -0,0 +1,23 @@
+function showPrompt(newScene) {
+    "use strict";
+
+    //Get the "next" and "replay" icons.
+    const next = document.getElementById("next");
+    const replay = document.getElementById("replay");
+
+    //Check scene number. Make icon ivisible or fade it in.
+    if (newScene === 0) {
+        next.className = "fade-in";
+        replay.className = "invisible";
+
+    } else if (newScene === 9) {
+        next.className = "invisible";
+        replay.className = "fade-in";
+
+    } else {
+        next.className = "invisible";
+        replay.className = "invisible";
+    }
+}
+
+export {showPrompt};
