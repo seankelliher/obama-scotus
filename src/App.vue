e3e9ce44a978ffb4185f987e924ce90bed8ff67f
Sean Kelliher <sbkelliher@icloud.com>
2024-07-08 08:44:07 -0400

    Modify components imported into App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <script setup>
-import GalleryScenes from "./components/GalleryScenes.vue";
+import HeaderPart from "./components/HeaderPart.vue";
+import ThumbImages from "./components/ThumbImages.vue";
+import S0BarackObama from "./components/S0BarackObama.vue";
+import S1JohnRoberts from "./components/S1JohnRoberts.vue";
+import S2SamuelAlito from "./components/S2SamuelAlito.vue";
+import S3SoniaSotomayor from "./components/S3SoniaSotomayor.vue";
+import S4ElenaKagan from "./components/S4ElenaKagan.vue";
+import S5AntoninScalia from "./components/S5AntoninScalia.vue";
+import S6MerrickGarland from "./components/S6MerrickGarland.vue";
+import S7MitchMcConnell from "./components/S7MitchMcConnell.vue";
+import S8NeilGorsuch from "./components/S8NeilGorsuch.vue";
 </script>
 
 <template>
-    <GalleryScenes />
+    <div id="container">
+        <HeaderPart />
+        <ThumbImages />
+        <S0BarackObama />
+        <S1JohnRoberts />
+        <S2SamuelAlito />
+        <S3SoniaSotomayor />
+        <S4ElenaKagan />
+        <S5AntoninScalia />
+        <S6MerrickGarland />
+        <S7MitchMcConnell />
+        <S8NeilGorsuch />
+    </div>
 </template>
 
 <style scoped>
