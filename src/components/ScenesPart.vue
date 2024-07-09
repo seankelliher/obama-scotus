de0c418cc35fee05203ff02eadd7ef22ef6a6855
Sean Kelliher <sbkelliher@icloud.com>
2024-07-09 09:50:24 -0400

    Add 'ScenesPart' component. Use for all scenes. Replaces ones for each justice.

---
+++ src/components/ScenesPart.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { store } from "../store.js";
+</script>
+
+<template>
+        <section>
+            <div class="sidebar">
+                <h2>{{ store.scenes[store.display].heading }}</h2>
+                <p>{{ store.scenes[store.display].para }}</p>
+            </div>
+
+                <figure class="large">
+                    <img :src="`/images/${store.scenes[store.display].imgScr}.jpg`" :alt="`${store.scenes[store.display].imgAlt}`">
+                    <figcaption>{{ store.scenes[store.display].caption }}</figcaption>
+                </figure>
+        </section>
+</template>
+
+<style scoped>
+</style>
