83eb50fd76d3e4b3dc5aa8e93a517b2224366bca
Sean Kelliher <sbkelliher@icloud.com>
2025-02-22 17:22:12 -0500

    Add new components, ContentBoxes.vue and FullWidth.vue.

---
+++ src/components/ContentBoxes.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { store } from "../store.js";
+import { onMounted } from "vue";
+
+onMounted(() => {
+    // Fades in callouts when user scrolls them into view.
+    const callouts = document.querySelectorAll(".content-box");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                /* const contains = entry.target.classList.contains("fade-in");
+                if (!contains) {
+                    entry.target.classList.add("fade-in");
+                } */
+                store.changeFullWidth(entry.target.id);
+            }
+        });
+    }, { threshold: 0.5 });
+
+    callouts.forEach(callout => observer.observe(callout));
+});
+</script>
+
+<template>
+        <section
+            v-for="scene in store.scenes"
+            :key="scene.id"
+        >
+            <div
+                :id="`${scene.id}`"
+                class="content-box"
+            >
+                <figure class="portrait">
+                    <img :src="`images/${scene.portScr}.jpg`" :alt="`${scene.alt}`">
+                    <figcaption>{{ scene.portCap }}</figcaption>
+                </figure>
+                <div class="content-box-text">
+                    <h2>{{ scene.contentTitle }}</h2>
+                    <p>{{ scene.contentText }}</p>
+                </div>
+            </div>
+        </section>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/FullWidth.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { store } from "../store.js";
+</script>
+
+<template>
+    <div class="full-width-container">
+        <div
+            v-if="store.fullWidth === 0"
+            class="full-width-title"
+        >
+            <h1>Barack Obama &amp; the Justices</h1>
+        </div>
+        <figure class="full-width">
+            <img
+                :src="`images/${store.scenes[store.fullWidth].fullSrc}.jpg`"
+                :alt="`${store.scenes[store.fullWidth].fullAlt}`"
+                class="full-width-img"
+            >
+            <figcaption class="full-width-caption">
+                {{ store.scenes[store.fullWidth].fullCap }}
+            </figcaption>
+
+        </figure>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
